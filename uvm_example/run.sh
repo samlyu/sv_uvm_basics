50434ebb933a328f96ee7f00fd3784f31fb3dbb0
samlyu <lvshengwei.samuel@gmail.com>
2019-12-09 18:03:05 +0800

    verdi run up 1

--- uvm_example/run.sh
+++ uvm_example/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export UVM_HOME=/home/eda/Synopsys/VCS2016/etc/uvm-1.1/
-vcs -full64 -debug_access+all -kdb -lca -sverilog -f filelist.f -ntb_opts uvm-1.1 +incdir+./ -y ./ +libext+.sv+.v -timescale=1ns/1ps -P $VERDI_HOME/share/PLI/VCS/LINUXAMD64/novas.tab $VERDI_HOME/share/PLI/VCS/LINUXAMD64/pli.a +vcs+dumpvars
+vcs -full64 -debug_access+all -kdb -lca -sverilog -f filelist.f \
+-ntb_opts uvm-1.1 +incdir+./ -y ./ +libext+.sv+.v -timescale=1ns/1ps \
+-P $VERDI_HOME/share/PLI/VCS/LINUXAMD64/novas.tab \
+$VERDI_HOME/share/PLI/VCS/LINUXAMD64/pli.a -fsdb
+
 ./simv +UVM_TESTNAME=$1 -gui=verdi
